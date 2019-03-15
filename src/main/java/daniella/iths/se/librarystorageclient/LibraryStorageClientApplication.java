9f5330d6b7e395cb73ac99f720876f1a5388d526
Daniella Noren <daniellanoren@live.se>
2019-03-15 16:46:52 +0100

    Initial commit

---
+++ src/main/java/daniella/iths/se/librarystorageclient/LibraryStorageClientApplication.java
@@ -0,0 +1,13 @@
+package daniella.iths.se.librarystorageclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryStorageClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryStorageClientApplication.class, args);
+    }
+
+}
---
+++ src/test/java/daniella/iths/se/librarystorageclient/LibraryStorageClientApplicationTests.java
@@ -0,0 +1,16 @@
+package daniella.iths.se.librarystorageclient;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LibraryStorageClientApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
