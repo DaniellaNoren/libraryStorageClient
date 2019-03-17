894b7b8444b6999c213320fcb019e8df0ed4ce76
Daniella Noren <daniellanoren@live.se>
2019-03-17 22:26:54 +0100

    Change URI to cloud

--- src/main/java/daniella/iths/se/librarystorageclient/communication/StorageCommunicator.java
+++ src/main/java/daniella/iths/se/librarystorageclient/communication/StorageCommunicator.java
@@ -26,7 +26,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
         ResponseEntity<ListOfObject<Book>> responseEntity;
         RequestEntity<Void> req;
         try {
-            req = RequestEntity.get(new URI("http://localhost:8081/books")).accept(MediaType.APPLICATION_JSON).build();
+            req = RequestEntity.get(new URI("http://library-storage.herokuapp.com/books")).accept(MediaType.APPLICATION_JSON).build();
             responseEntity = restTemplate.exchange(req, new ParameterizedTypeReference<ListOfObject<Book>>(){});
             return responseEntity.getBody().getList();
         } catch (URISyntaxException e) {
@@ -38,7 +38,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
     public List<Author> getAllAuthors(){
         ResponseEntity<ListOfObject<Author>> responseEntity;
         try{
-            responseEntity = restTemplate.exchange(new URI("http://localhost:8081/authors"), HttpMethod.GET, null, new ParameterizedTypeReference<ListOfObject<Author>>() {});
+            responseEntity = restTemplate.exchange(new URI("http://library-storage.herokuapp.com/authors"), HttpMethod.GET, null, new ParameterizedTypeReference<ListOfObject<Author>>() {});
             return responseEntity.getBody().getList();
         }catch(URISyntaxException e){
             e.printStackTrace();
@@ -50,7 +50,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
     public Book getOneBook(long id) {
         ResponseEntity<Book> responseEntity;
         try{
-            responseEntity = restTemplate.exchange(new URI("http://localhost:8081/books/"+id), HttpMethod.GET, null, Book.class);
+            responseEntity = restTemplate.exchange(new URI("http://library-storage.herokuapp.com/books/"+id), HttpMethod.GET, null, Book.class);
             return responseEntity.getBody();
         }catch(URISyntaxException e){
             e.printStackTrace();
@@ -64,7 +64,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
     public Author getOneAuthor(long id) {
         ResponseEntity<Author> res;
         try{
-            res = restTemplate.exchange(new URI("http://localhost:8081/authors/"+id), HttpMethod.GET, null, Author.class);
+            res = restTemplate.exchange(new URI("http://library-storage.herokuapp.com/authors/"+id), HttpMethod.GET, null, Author.class);
             return res.getBody();
         }catch(URISyntaxException e){
             e.printStackTrace();
@@ -80,7 +80,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
         RequestEntity<AuthorAttributes> req;
         ResponseEntity<Author> res;
         try{
-            req = RequestEntity.put(new URI("http://localhost:8081/authors/"+author_id)).accept(MediaType.APPLICATION_JSON).body(authorAttributes);
+            req = RequestEntity.put(new URI("http://library-storage.herokuapp.com/authors/"+author_id)).accept(MediaType.APPLICATION_JSON).body(authorAttributes);
             res = restTemplate.exchange(req, Author.class);
             return res.getBody();
 
@@ -97,7 +97,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
         RequestEntity<BookAttributes> req;
         ResponseEntity<Book> responseEntity;
         try {
-           req = RequestEntity.post(new URI("http://localhost:8081/books")).accept(MediaType.APPLICATION_JSON).body(bookAttributes);
+           req = RequestEntity.post(new URI("http://library-storage.herokuapp.com/books")).accept(MediaType.APPLICATION_JSON).body(bookAttributes);
            responseEntity = restTemplate.exchange(req, Book.class);
            return responseEntity.getBody();
 
@@ -112,7 +112,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
         RequestEntity<BookAttributes> req;
         ResponseEntity<Book> res;
         try{
-            req = RequestEntity.put(new URI("http://localhost:8081/books/"+id)).accept(MediaType.APPLICATION_JSON).body(b);
+            req = RequestEntity.put(new URI("http://library-storage.herokuapp.com/books/"+id)).accept(MediaType.APPLICATION_JSON).body(b);
             res = restTemplate.exchange(req, Book.class);
             return res.getBody();
 
@@ -129,7 +129,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
         RequestEntity<AuthorAttributes> req;
         ResponseEntity<Author> responseEntity;
         try {
-        req = RequestEntity.post(new URI("http://localhost:8081/authors")).accept(MediaType.APPLICATION_JSON).body(authorAttributes);
+        req = RequestEntity.post(new URI("http://library-storage.herokuapp.com/authors")).accept(MediaType.APPLICATION_JSON).body(authorAttributes);
         responseEntity = restTemplate.exchange(req, Author.class);
         return responseEntity.getBody();
 
@@ -145,7 +145,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
         RequestEntity req;
         String mes = "";
         try{
-            req = RequestEntity.delete(new URI("http://localhost:8081/authors/delete/"+author_id)).accept(MediaType.APPLICATION_JSON).build();
+            req = RequestEntity.delete(new URI("http://library-storage.herokuapp.com/authors/delete/"+author_id)).accept(MediaType.APPLICATION_JSON).build();
             res = restTemplate.exchange(req, String.class);
             return mes = res.getBody();
 
@@ -161,7 +161,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
         ResponseEntity<String> res;
 
         try{
-            res = restTemplate.exchange(new URI("http://localhost:8081/books/delete/"+id), HttpMethod.DELETE, null, String.class);
+            res = restTemplate.exchange(new URI("http://library-storage.herokuapp.com/books/delete/"+id), HttpMethod.DELETE, null, String.class);
         }catch(URISyntaxException e){
             e.printStackTrace();
         }
@@ -172,7 +172,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
         RequestEntity<Long> req;
         ResponseEntity<Book> res;
         try{
-            req = RequestEntity.put(new URI("http://localhost:8081/books/borrow/"+id)).accept(MediaType.APPLICATION_JSON).body(userId);
+            req = RequestEntity.put(new URI("http://library-storage.herokuapp.com/books/borrow/"+id)).accept(MediaType.APPLICATION_JSON).body(userId);
             res = restTemplate.exchange(req, Book.class);
             return res.getBody();
         }catch(URISyntaxException e){
@@ -187,7 +187,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
     public Book returnBook(long id) {
         ResponseEntity<Book> res;
         try{
-            res = restTemplate.exchange(new URI("http://localhost:8081/books/return/"+id), HttpMethod.PUT, null, Book.class);
+            res = restTemplate.exchange(new URI("http://library-storage.herokuapp.com/books/return/"+id), HttpMethod.PUT, null, Book.class);
             return res.getBody();
         }catch(URISyntaxException e){
 
@@ -201,7 +201,7 @@ public class StorageCommunicator implements BookStorage, AuthorStorage {
     public User getUser(long id) {
         ResponseEntity<User> res;
         try{
-            res = restTemplate.exchange(new URI("http://localhost:8081/books/"+id+"/users"), HttpMethod.GET, null, User.class);
+            res = restTemplate.exchange(new URI("http://library-storage.herokuapp.com/books/"+id+"/users"), HttpMethod.GET, null, User.class);
             return res.getBody();
         }catch(URISyntaxException e){
 
